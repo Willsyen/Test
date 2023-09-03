8d80f3210ce48317a0822676fcf5eafcdb1923f7
Willsyen <yen.ratha9@gmail.com>
2023-09-03 17:14:33 +0700

    Add Vue Lession

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      students:[
+       {name:'Ratha Yen',age:28},
+       {name:'Sorphorn',age:26}
+     
+      ]
+    },
+    methods:{
+      
+    }
+  })
+  // ========================================end===============================
+  var app1 = new Vue({
+    el: '#app1',
+    data:{
+      message:'',
+      checked:'',
+      sex:[],
+      picked:[],
+      selected:''
+
+    },
+    methods:{
+
+    }
+  })
